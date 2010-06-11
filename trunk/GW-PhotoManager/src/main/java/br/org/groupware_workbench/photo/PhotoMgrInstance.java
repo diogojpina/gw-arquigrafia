99138b6b79a9b4a5ba8a0c638596e0d4a1141b58
gcc.carlos.cc <gcc.carlos.cc@3a9e3eec-87af-96a1-6a36-76b78f6a347c>
2010-06-11 03:24:38 +0000

    specific-configuration do PhotoMgr

--- trunk/GW-PhotoManager/src/main/java/br/org/groupware_workbench/photo/PhotoMgrInstance.java
+++ trunk/GW-PhotoManager/src/main/java/br/org/groupware_workbench/photo/PhotoMgrInstance.java
@@ -11,105 +11,90 @@ import br.org.groupware_workbench.collabletFw.facade.CollabletInstance;
 import br.org.groupware_workbench.coreutils.DAOFactory;
 import br.org.groupware_workbench.coreutils.GenericEntity;
 import br.org.groupware_workbench.photo.internal.PhotoDAO;
+import br.org.groupware_workbench.photo.internal.PhotoMgrComponent;
 
 public class PhotoMgrInstance extends CollabletInstance {
 
-	private final PhotoDAO dao = DAOFactory.get(PhotoDAO.class);
+    private final PhotoDAO dao = DAOFactory.get(PhotoDAO.class);
 
-	// TODO: Não deve haver estado mutável transiente nesta classe, visto que ela é compartilhada entre diferentes
-	// Threads.
-	private String dirImagesRelativo = "images";
-	private String cropPrefix = "crop_";
-	private	String thumbPrefix = "thumb_";
-	private String mostraPrefix = "mostra_";
-	private String dirImagesAbsoluto = null;
+    // TODO: Não deve haver estado mutável transiente nesta classe, visto que ela é compartilhada entre diferentes
+    // Threads.
+    
+    private String dirImagesAbsoluto = null;
 
-	public PhotoMgrInstance() {
-		super();
-		this.thumbPrefix = "thumb_";
-		this.setCropPrefix("crop_");
-		this.mostraPrefix = "mostra_";
-	}
+    public PhotoMgrInstance() {
+        super();
+    }
 
-	public void setRequestInfo(RequestInfo info) {
-		dirImagesAbsoluto = info.getServletContext().getRealPath("/") + dirImagesRelativo + File.separator;
-	}
+    public void setRequestInfo(RequestInfo info) {
+        if(dirImagesAbsoluto==null){
+            dirImagesAbsoluto = info.getServletContext().getRealPath("/") + this.getDirImagesRelativo() + File.separator;
+            File dir=new File(dirImagesAbsoluto);
+            if(!dir.exists()){
+                dir.mkdir();
+            }
+        }
+    }
 
-	@Override
-	public void destroy() {
-		this.dao.deleteByIdInstance(this.getId());
-	}
+    @Override
+    public void destroy() {
+        this.dao.deleteByIdInstance(this.getId());
+    }
 
-	public void delete(long photoId) {
-		dao.deleteById(photoId);
-	}
+    public void delete(long photoId) {
+        dao.deleteById(photoId);
+    }
 
-	public void save(Photo photoRegister) {
-		photoRegister.setIdInstance(this.getId());
-		this.dao.save(photoRegister, true);
-	}
+    public void save(Photo photoRegister) {
+        photoRegister.setIdInstance(this.getId());
+        this.dao.save(photoRegister, true);
+    }
 
-	public void saveImage(InputStream foto, String nome) throws IOException {
-		this.dao.saveImage(foto, nome, dirImagesAbsoluto);
-	}
+    public void saveImage(InputStream foto, String nome) throws IOException {
+        this.dao.saveImage(foto, nome, dirImagesAbsoluto);
+    }
 
-	public List<Photo> buscaFoto(String busca) {
-		return this.dao.busca(busca,this.getId());
-	}
+    public List<Photo> buscaFoto(String busca) {
+        return this.dao.busca(busca,this.getId());
+    }
 
-	public List<Photo> buscaFotoAvancada(String nome, String descricao, String lugar, Date date) {
-		return this.dao.busca(nome, lugar, descricao, date, this.getId());
-	}
+    public List<Photo> buscaFotoAvancada(String nome, String descricao, String lugar, Date date) {
+        return this.dao.busca(nome, lugar, descricao, date, this.getId());
+    }
 
-	public Photo buscaPhotoById(long idPhoto) {
-		return this.dao.findById(idPhoto);
-	}
+    public Photo buscaPhotoById(long idPhoto) {
+        return this.dao.findById(idPhoto);
+    }
 
-	public List<Photo> buscaFotoPorListaId(List<GenericEntity> idList) {
-	    return this.dao.buscaPorID(idList, this.getId());
-	}
+    public List<Photo> buscaFotoPorListaId(List<GenericEntity> idList) {
+        return this.dao.buscaPorID(idList, this.getId());
+    }
 
-	public List<Photo> listaTodaPhoto() {
-		return dao.listAll();
-	}
-	
-	public List<Photo> listaPhotoPorPaginaEOrdem(int tamanho, int pagina) {
+    public List<Photo> listaTodaPhoto() {
+        return dao.listAll();
+    }
+    
+    public List<Photo> listaPhotoPorPaginaEOrdem(int tamanho, int pagina) {
         return dao.listPhotoByPageAndOrder(tamanho,pagina);
     }
 
-	public String getDirImagesRelativo() {
-		return dirImagesRelativo;
-	}
+    public String getDirImagesRelativo() {      
+        return ((PhotoMgrComponent)this.getComponent()).getDirImages();
+    }
 
-	public String getThumbPrefix() {
-		return thumbPrefix;
-	}
+    public String getThumbPrefix() {        
+        return ((PhotoMgrComponent)this.getComponent()).getThumbPrefix();
+    }
 
-	public void setThumbPrefix(String thumbPrefix) {
-		this.thumbPrefix = thumbPrefix;
-	}
+    public String getMostraPrefix() {
+        return ((PhotoMgrComponent)this.getComponent()).getMostraPrefix();      
+    }
+    
+    public String getDirImagesAbsoluto() {      
+        return dirImagesAbsoluto;
+    }
 
-	public String getMostraPrefix() {
-		return mostraPrefix;
-	}
-
-	public void setMostraPrefix(String mostraPrefix) {
-		this.mostraPrefix = mostraPrefix;
-	}
-
-	public void setDirImagesRelativo(String dirImagesRelativo) {
-		this.dirImagesRelativo = dirImagesRelativo;
-	}
-
-	public String getDirImagesAbsoluto() {
-		return dirImagesAbsoluto;
-	}
-
-	public void setCropPrefix(String cropPrefix) {
-		this.cropPrefix = cropPrefix;
-	}
-
-	public String getCropPrefix() {
-		return cropPrefix;
-	}
+    public String getCropPrefix() {     
+        return ((PhotoMgrComponent)this.getComponent()).getCropPrefix();
+    }
 }
--- trunk/GW-PhotoManager/src/main/java/br/org/groupware_workbench/photo/internal/PhotoMgrComponent.java
+++ trunk/GW-PhotoManager/src/main/java/br/org/groupware_workbench/photo/internal/PhotoMgrComponent.java
@@ -1,5 +1,11 @@
 package br.org.groupware_workbench.photo.internal;
 
+import java.util.List;
+
+import org.dom4j.Document;
+import org.dom4j.Element;
+import org.dom4j.Node;
+
 import br.org.groupware_workbench.collabletFw.facade.Collablet;
 import br.org.groupware_workbench.coreutils.ComponentInfo;
 import br.org.groupware_workbench.photo.PhotoMgrInstance;
@@ -10,5 +16,68 @@ import br.org.groupware_workbench.photo.PhotoMgrInstance;
     configurationURL="/groupware-workbench/{photoInstance}/photo"
 )
 public class PhotoMgrComponent extends Collablet {
+    private String dirImages = "images";
+    private String cropPrefix = "crop_";
+    private String thumbPrefix = "thumb_";
+    private String mostraPrefix = "mostra_";
+           
+    public PhotoMgrComponent(){
+                
+    }
+    
+    @SuppressWarnings({"unchecked", "null"})
+    @Override
+    protected void applyConfiguration(Element xml, boolean isInstalling){
+        Document doc=xml.getDocument();        
+        List<Node> imageDirectory=doc.selectNodes("//collablet/specific-configuration/image-directory");        
+        List<Node> show=doc.selectNodes("//collablet/specific-configuration/show-prefix");
+        List<Node> crop=doc.selectNodes("//collablet/specific-configuration/crop-prefix");
+        List<Node> thumb=doc.selectNodes("//collablet/specific-configuration/thumbnail-prefix");
+        
+        if(imageDirectory!=null){
+            dirImages=imageDirectory.get(0).valueOf("@value");
+        }
+        if(show!=null){
+            mostraPrefix=show.get(0).valueOf("@value");
+        }
+        if(crop!=null){
+            cropPrefix=crop.get(0).valueOf("@value");
+        }
+        if(thumb!=null){
+            thumbPrefix=thumb.get(0).valueOf("@value");
+        }                      
+    }
+
+    public String getDirImages() {
+        return dirImages;
+    }
+
+    public void setDirImages(String dirImages) {
+        this.dirImages = dirImages;
+    }
+
+    public String getCropPrefix() {
+        return cropPrefix;
+    }
+
+    public void setCropPrefix(String cropPrefix) {
+        this.cropPrefix = cropPrefix;
+    }
+
+    public String getThumbPrefix() {
+        return thumbPrefix;
+    }
+
+    public void setThumbPrefix(String thumbPrefix) {
+        this.thumbPrefix = thumbPrefix;
+    }
+
+    public String getMostraPrefix() {
+        return mostraPrefix;
+    }
+
+    public void setMostraPrefix(String mostraPrefix) {
+        this.mostraPrefix = mostraPrefix;
+    }
 
 }
