28bd77f980d5b5174fe201a23856e7acfcc626c9
lucasant <lucasant@3a9e3eec-87af-96a1-6a36-76b78f6a347c>
2010-07-09 22:20:22 +0000

    aplicação da licença LGL3 nos arquivos java.

--- trunk/GW-PhotoManager/src/test/java/br/org/groupwareworkbench/arquigrafia/photo/PhotoControllerTest.java
+++ trunk/GW-PhotoManager/src/test/java/br/org/groupwareworkbench/arquigrafia/photo/PhotoControllerTest.java
@@ -1,3 +1,22 @@
+/*
+*    UNIVERSIDADE DE SÃO PAULO.
+*    Author: Marco Aurélio Gerosa (gerosa@ime.usp.br)
+*
+*    This file is part of Groupware Workbench (http://www.groupwareworkbench.org.br).
+*
+*    Groupware Workbench is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU Lesser General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    (at your option) any later version.
+*
+*    Groupware Workbench is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU Lesser General Public License for more details.
+*
+*    You should have received a copy of the GNU Lesser General Public License
+*    along with Swift.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package br.org.groupwareworkbench.arquigrafia.photo;
 
 import static org.mockito.Mockito.mock;
