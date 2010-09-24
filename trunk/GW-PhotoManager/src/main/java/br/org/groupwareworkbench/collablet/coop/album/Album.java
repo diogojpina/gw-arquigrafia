09cd60e34aca8c9bd52a9b0488a7e52f920c0392
vphocco <vphocco@3a9e3eec-87af-96a1-6a36-76b78f6a347c>
2010-09-24 22:20:47 +0000

    Componente Album, AlbumMgrInstance, AlbumMgrController e testes

--- trunk/GW-PhotoManager/src/main/java/br/org/groupwareworkbench/collablet/coop/album/Album.java
+++ trunk/GW-PhotoManager/src/main/java/br/org/groupwareworkbench/collablet/coop/album/Album.java
@@ -1,25 +1,27 @@
 /*
-*    UNIVERSIDADE DE SÃO PAULO.
-*    Author: Marco Aurélio Gerosa (gerosa@ime.usp.br)
-*
-*    This file is part of Groupware Workbench (http://www.groupwareworkbench.org.br).
-*
-*    Groupware Workbench is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU Lesser General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    Groupware Workbench is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU Lesser General Public License for more details.
-*
-*    You should have received a copy of the GNU Lesser General Public License
-*    along with Swift.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *    UNIVERSIDADE DE SÃO PAULO.
+ *    Author: Marco Aurélio Gerosa (gerosa@ime.usp.br)
+ *
+ *    This file is part of Groupware Workbench (http://www.groupwareworkbench.org.br).
+ *
+ *    Groupware Workbench is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Groupware Workbench is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with Swift.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package br.org.groupwareworkbench.collablet.coop.album;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -31,15 +33,20 @@ import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.persistence.UniqueConstraint;
 
 import br.org.groupwareworkbench.arquigrafia.photo.Photo;
 import br.org.groupwareworkbench.collablet.coord.user.User;
+import br.org.groupwareworkbench.core.bd.GenericReference;
 import br.org.groupwareworkbench.core.bd.ObjectDAO;
 import br.org.groupwareworkbench.core.framework.Collablet;
 
 @Entity
-@Table(name="gw_collab_Album")
-public class Album implements Serializable  {
+@Table(
+        name = "gw_collab_Album",
+        uniqueConstraints={@UniqueConstraint(columnNames={"collablet_id", "title"})}
+)
+public class Album implements Serializable {
 
     private static final long serialVersionUID = -6876191440831919034L;
 
@@ -62,21 +69,26 @@ public class Album implements Serializable  {
 
     @ManyToOne
     private User owner;
-    
+
     @OneToMany
-    private List<Photo> photos;
+    private List<Photo> photos = new ArrayList<Photo>();
 
     public Album() {
     }
 
     /* Operations */
-    
+
     public void save() {
         DAO.save(this);
     }
 
     public void delete() {
         DAO.delete(this);
+    }
+
+    public boolean contains(Photo photo) {
+        if (photo == null) throw new IllegalArgumentException();
+        return this.photos.contains(photo);
     }
 
     public static Album findById(Long id) {
@@ -91,31 +103,27 @@ public class Album implements Serializable  {
     public static Album findByName(String name, Collablet collablet) {
         if (collablet == null) throw new IllegalArgumentException();
         if (name == null) throw new IllegalArgumentException();
-        return DAO.query()
-                    .with("collablet", collablet)
-                    .with("name", name)
-                    .find();
+        return DAO.query().with("collablet", collablet).with("name", name).find();
     }
-    
+
     // Add photos
     public void add(Photo photo) {
         if (photo == null) throw new IllegalArgumentException();
         this.photos.add(photo);
     }
-    
+
     // Delete photos
     public void remove(Photo photo) {
         if (photo == null) throw new IllegalArgumentException();
         this.photos.remove(photo);
     }
-    
-    // List the last photos
-    
-    // Sort photos
-    
 
-    /* Getters and Setters*/
-    
+    // List the last photos
+
+    // Sort photos
+
+    /* Getters and Setters */
+
     public Long getId() {
         return id;
     }
@@ -144,11 +152,19 @@ public class Album implements Serializable  {
         this.updateDate = updateDate == null ? null : (Date) updateDate.clone();
     }
 
+    public List<Photo> getPhotos() {
+        return Collections.unmodifiableList(this.photos);
+    }
+
     public Collablet getCollablet() {
         return collablet;
     }
 
     public void setCollablet(Collablet collablet) {
         this.collablet = collablet;
+    }
+
+    public int getSize() {
+        return this.photos.size();
     }
 }
--- trunk/GW-PhotoManager/src/main/java/br/org/groupwareworkbench/collablet/coop/album/AlbumMgrController.java
+++ trunk/GW-PhotoManager/src/main/java/br/org/groupwareworkbench/collablet/coop/album/AlbumMgrController.java
@@ -19,17 +19,80 @@
  */
 package br.org.groupwareworkbench.collablet.coop.album;
 
+import java.util.Collection;
+
+import br.com.caelum.vraptor.Delete;
+import br.com.caelum.vraptor.Get;
+import br.com.caelum.vraptor.Path;
+import br.com.caelum.vraptor.Post;
 import br.com.caelum.vraptor.Resource;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.ioc.RequestScoped;
+import br.com.caelum.vraptor.view.Results;
+import br.org.groupwareworkbench.core.framework.Collablet;
 
 @RequestScoped
 @Resource
 public class AlbumMgrController {
 
-    //private final Result result;
+    private final Result result;
 
-    public AlbumMgrController(/*Result result*/) {
-        //this.result = result;
+    public AlbumMgrController(Result result) {
+        this.result = result;
     }
+    
+    
+    @Get
+    @Path(value="/groupware-workbench/{collablet}/albumMgr/{albumMgr}")
+    public void list(final Collablet collablet, final AlbumMgrInstance albumMgr) {     
+        result.include("collabletInstance", collablet);
+        Collection<Album> albumList = albumMgr.list();
+        result.include("albumList", albumList);
+        result.use(Results.representation()).from(albumList).serialize();
+    }
+
+    @Get
+    @Path(value="/groupware-workbench/{collablet}/albumMgr/{albumMgr}/create")
+    public void create(final Collablet collablet, final AlbumMgrInstance albumMgr) {
+        Album album = new Album();
+        result.include("collabletInstance", collablet);
+        result.include("album", album);
+        result.use(Results.representation()).from(album).serialize();
+    }
+
+    @Get
+    @Path(value="/groupware-workbench/{collablet}/albumMgr/{albumMgr}/{id}")
+    public void retrieve(final Collablet collablet, final AlbumMgrInstance albumMgr, final long id) {
+        Album album = Album.findById(id);
+        if (album == null) {
+            this.result.notFound();
+            return;
+        }
+
+        result.include("collabletInstance", collablet);
+        result.include("album", album);
+        result.use(Results.representation()).from(album).serialize();
+    }
+
+    @Post
+    @Path(value="/groupware-workbench/{collablet}/albumMgr/{albumMgr}")
+    public void save(final Collablet collablet, AlbumMgrInstance albumMgr, Album album) {
+        albumMgr.save(album);
+        result.use(Results.logic()).redirectTo(AlbumMgrController.class).list(collablet, albumMgr);
+    }
+
+    @Delete
+    @Path(value="/groupware-workbench/{collablet}/albumMgr/{albumMgr}/{id}")
+    public void delete(Collablet collablet, AlbumMgrInstance albumMgr, final long id) {
+        Album album = Album.findById(id);
+        if (album != null) album.delete();
+        result.use(Results.logic()).redirectTo(AlbumMgrController.class).list(collablet, albumMgr);
+    }
+
+    /*@Get
+    @Path(value="/groupware-workbench/{collablet}/albumMgr/{albumMgr}/resource/{resource*}")
+    public File getResource(AlbumMgrInstance albumMgr, String resource) {
+        return albumMgr.resourcePath(resource);
+    }*/
+
 }
