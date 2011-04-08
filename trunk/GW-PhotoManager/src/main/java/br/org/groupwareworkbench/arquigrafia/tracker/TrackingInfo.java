2fb9b71f2fd71d2b459b69a568c2a5331d305b42
spideybr <spideybr@3a9e3eec-87af-96a1-6a36-76b78f6a347c>
2011-04-08 13:46:53 +0000

    Formatação, basicamente.

--- trunk/GW-PhotoManager/src/main/java/br/org/groupwareworkbench/arquigrafia/tracker/TrackingInfo.java
+++ trunk/GW-PhotoManager/src/main/java/br/org/groupwareworkbench/arquigrafia/tracker/TrackingInfo.java
@@ -1,23 +1,23 @@
 /*
-*    UNIVERSIDADE DE SÃO PAULO.
-*    Author: Marco Aurélio Gerosa (gerosa@ime.usp.br)
-*    This project was/is sponsored by RNP and FAPESP.
-*
-*    This file is part of Groupware Workbench (http://www.groupwareworkbench.org.br).
-*
-*    Groupware Workbench is free software: you can redistribute it and/or modify
-*    it under the terms of the GNU Lesser General Public License as published by
-*    the Free Software Foundation, either version 3 of the License, or
-*    (at your option) any later version.
-*
-*    Groupware Workbench is distributed in the hope that it will be useful,
-*    but WITHOUT ANY WARRANTY; without even the implied warranty of
-*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*    GNU Lesser General Public License for more details.
-*
-*    You should have received a copy of the GNU Lesser General Public License
-*    along with Swift.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ *    UNIVERSIDADE DE SÃO PAULO.
+ *    Author: Marco Aurélio Gerosa (gerosa@ime.usp.br)
+ *    This project was/is sponsored by RNP and FAPESP.
+ *
+ *    This file is part of Groupware Workbench (http://www.groupwareworkbench.org.br).
+ *
+ *    Groupware Workbench is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU Lesser General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    Groupware Workbench is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU Lesser General Public License for more details.
+ *
+ *    You should have received a copy of the GNU Lesser General Public License
+ *    along with Swift.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package br.org.groupwareworkbench.arquigrafia.tracker;
 
 import java.io.Serializable;
@@ -38,76 +38,76 @@ import br.org.groupwareworkbench.core.framework.Collablet;
 @Entity
 public class TrackingInfo implements Serializable {
     private static final long serialVersionUID = -7858458259300455392L;
-    
+
     private static final ObjectDAO<TrackingInfo, Long> DAO = new ObjectDAO<TrackingInfo, Long>(TrackingInfo.class);
-    
+
     @Id
-	@GeneratedValue
-	private Long id;
+    @GeneratedValue
+    private Long id;
 
     @ManyToOne
-    @JoinColumn(name="collablet_id")
+    @JoinColumn(name = "collablet_id")
     private Collablet collablet;
-    
+
     @ManyToOne
-	private User user;
-	
-	private double accuracy;
-	
-	private double latitude;
-	
-	private double longitude;
-	
-	@Temporal(TemporalType.TIMESTAMP)
-	private Date dateUpdate;
-	
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
+    private User user;
 
-	public User getUser() {
-		return user;
-	}
-	
-	public void setUser(User user) {
-		this.user = user;
-	}
-	
-	public double getAccuracy() {
-		return accuracy;
-	}
-	
-	public void setAccuracy(double accuracy) {
-		this.accuracy = accuracy;
-	}
-	
-	public double getLatitude() {
-		return latitude;
-	}
-	
-	public void setLatitude(double latitude) {
-		this.latitude = latitude;
-	}
-	
-	public double getLongitude() {
-		return longitude;
-	}
-	
-	public void setLongitude(double longitude) {
-		this.longitude = longitude;
-	}
+    private double accuracy;
 
-	public Date getDateUpdate() {
-		return dateUpdate;
-	}
-	
-	public void setDateUpdate(Date dateUpdate) {
-		this.dateUpdate = dateUpdate;
-	}
+    private double latitude;
+
+    private double longitude;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date dateUpdate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public double getAccuracy() {
+        return accuracy;
+    }
+
+    public void setAccuracy(double accuracy) {
+        this.accuracy = accuracy;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public Date getDateUpdate() {
+        return dateUpdate;
+    }
+
+    public void setDateUpdate(Date dateUpdate) {
+        this.dateUpdate = dateUpdate;
+    }
 
     public Collablet getCollablet() {
         return collablet;
@@ -117,16 +117,15 @@ public class TrackingInfo implements Serializable {
         this.collablet = collablet;
     }
 
-    public void save(){
+    public void save() {
         DAO.save(this);
     }
-    
-    public void delete(){
+
+    public void delete() {
         DAO.delete(this);
     }
-    
-    public static TrackingInfo find(long id) 
-    {
+
+    public static TrackingInfo find(long id) {
         return DAO.findById(id);
     }
 }
