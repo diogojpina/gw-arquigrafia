14716821a9d61b1aa1794c8b5f80a6e6f43b0995
strausmm <strausmm@3a9e3eec-87af-96a1-6a36-76b78f6a347c>
2012-03-07 20:54:55 +0000

    Adição dos scripts, css e imagens novos e divisão do header e footer.

---
+++ trunk/GW-Application-Arquigrafia/src/main/webapp/js/index.js
@@ -0,0 +1,52 @@
+// JavaScript Document
+form_window_loaded = false;
+
+$(document).ready(function(){
+	
+	$('#registration_button').click(function(){
+		$('#mask').fadeIn('fast');
+		$('#form_window').fadeIn('slow');
+		if (!form_window_loaded) $('#registration').load('modal/signup.html');	
+		form_window_loaded = true;
+	});
+	
+	$('#upload').click(function(){
+		$('#mask').fadeIn('fast');
+		$('#form_window').fadeIn('slow');
+		if (!form_window_loaded) $('#registration').load('modal/upload.html');	
+		form_window_loaded = true;
+	});
+	
+	$('#upload_bar').click(function(){
+		$('#mask').fadeIn('fast');
+		$('#form_window').fadeIn('slow');
+		if (!form_window_loaded) $('#registration').load('modal/upload_bar.html');	
+		form_window_loaded = true;
+	});
+	
+	$('#contact').click(function(){
+		$('#mask').fadeIn('fast');
+		$('#form_window').fadeIn('slow');
+		if (!form_window_loaded) $('#registration').load('modal/contact_us.html');	
+		form_window_loaded = true;
+	});
+	
+	$('#form_window .close').click(function (e) {
+		e.preventDefault();
+		
+		$('#mask').fadeOut();
+		$('#form_window').fadeOut('fast');
+	});		
+	
+	$('#mask').click(function () {
+		$(this).fadeOut();
+		$('#form_window').fadeOut('fast');
+	});
+	
+	$('#printer_icon').click(function() {
+		window.print();
+		return false;
+	});
+
+		
+});
