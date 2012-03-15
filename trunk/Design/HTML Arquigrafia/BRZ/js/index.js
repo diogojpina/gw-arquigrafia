ea6bd43e68191fd07bef1d5cc67b7d850e7ab369
strausmm <strausmm@3a9e3eec-87af-96a1-6a36-76b78f6a347c>
2012-03-15 19:12:55 +0000

    Melhorias no layout utulizadas na integração.

--- trunk/Design/HTML Arquigrafia/BRZ/js/index.js
+++ trunk/Design/HTML Arquigrafia/BRZ/js/index.js
@@ -10,6 +10,34 @@ $(document).ready(function(){
 		form_window_loaded = true;
 	});
 	
+	$('#upload').click(function(){
+		$('#mask').fadeIn('fast');
+		$('#form_window').fadeIn('slow');
+		if (!form_window_loaded) $('#registration').load('modal/upload.html');	
+		form_window_loaded = true;
+	});
+	
+	$('#upload_bar').click(function(){
+		$('#mask').fadeIn('fast');
+		$('#form_window').fadeIn('slow');
+		if (!form_window_loaded) $('#registration').load('modal/upload_bar.html');	
+		form_window_loaded = true;
+	});
+	
+	$('#contact').click(function(){
+		$('#mask').fadeIn('fast');
+		$('#form_window').fadeIn('slow');
+		if (!form_window_loaded) $('#registration').load('modal/contact_us.html');	
+		form_window_loaded = true;
+	});
+	
+	$('#login_button').click(function(){
+		$('#mask').fadeIn('fast');
+		$('#form_window').fadeIn('slow');
+		if (!form_window_loaded) $('#registration').load('modal/login.html');	
+		form_window_loaded = true;
+	});
+	
 	$('#form_window .close').click(function (e) {
 		e.preventDefault();
 		
@@ -22,10 +50,14 @@ $(document).ready(function(){
 		$('#form_window').fadeOut('fast');
 	});
 	
+	$('#complete_search').click(function() {
+	  $('#complete_search_bar').slideToggle('slow', function() {
+	  });
+	});
+	
 	$('#printer_icon').click(function() {
-		window.print();
-		return false;
+	  window.print();
+	  return false;
 	});
 
-		
 });
