54eeda2cdb63ff537a14f100f10d2c89e09ac626
joseteodoro1105 <joseteodoro1105@3a9e3eec-87af-96a1-6a36-76b78f6a347c>
2013-04-25 17:05:08 +0000

    extend ods reader tests.

--- trunk/GW-collablets-Arquigrafia/src/test/java/br/org/groupwareworkbench/arquigrafia/imports/OdsReaderTest.java
+++ trunk/GW-collablets-Arquigrafia/src/test/java/br/org/groupwareworkbench/arquigrafia/imports/OdsReaderTest.java
@@ -8,19 +8,31 @@ import static org.junit.Assert.*;
 
 public class OdsReaderTest {
 
-    private static final String resourceFilePath = "./src/test/resources/br/org/groupwareworkbench/arquigrafia/imports/planilha.ods";
+    private static final String smallResourceFilePath = "./src/test/resources/br/org/groupwareworkbench/arquigrafia/imports/small.ods";
+    private static final String largeResourceFilePath = "./src/test/resources/br/org/groupwareworkbench/arquigrafia/imports/large.ods";
+    private static final String manySheetsResourceFilePath = "./src/test/resources/br/org/groupwareworkbench/arquigrafia/imports/largeManySheets.ods";
     
     @Test
     public void test() {
         
-        File odsTestResourceFile = new File(resourceFilePath);
+        testForFile(smallResourceFilePath);
+        testForFile(largeResourceFilePath);
+        testForFile(manySheetsResourceFilePath);
+        
+    }
+
+    private void testForFile(String filePath) {
+        ArquigrafiaOdsReader imagesMetadataReader = setupArquigrafiaOdsFileReader(filePath);
+        Collection<ArquigrafiaImageMetadata> metadataImages = imagesMetadataReader.read();
+        assertTrue(!metadataImages.isEmpty() );
+        System.out.println(String.format("Ods File %s has %d entries.", filePath ,metadataImages.size()));
+    }
+
+    private ArquigrafiaOdsReader setupArquigrafiaOdsFileReader(String filePath) {
+        File odsTestResourceFile = new File(filePath);
         assertTrue( odsTestResourceFile.exists() );
         ArquigrafiaOdsReader imagesMetadataReader = new ArquigrafiaOdsReader(odsTestResourceFile);
-        assertTrue(imagesMetadataReader.getSheetCount() == 1 );
-        Collection<ArquigrafiaImageMetadata> metadataImages = imagesMetadataReader.read();
-        System.out.println(metadataImages.size());
-        assertTrue(metadataImages.size() == 3 );
-        
+        return imagesMetadataReader;
     }
     
 }
